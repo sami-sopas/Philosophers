c0559a8208e8d93e3cf52f285ce32f32b66d89ba
Samuel Cuenca <ramirezcuenca@outlook.es>
2024-03-25 18:46:15 -0600

    async background updating

--- Philosophers/Form1.Designer.cs
+++ Philosophers/Form1.Designer.cs
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.startButton = new System.Windows.Forms.Button();
+            this.BW_UpdateForm = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(616, 55);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(112, 36);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Comenzar";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1299, 627);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.ComponentModel.BackgroundWorker BW_UpdateForm;
     }
 }
 
