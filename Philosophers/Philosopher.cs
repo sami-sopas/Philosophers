afcc4e65dc01fb9c8394dda2102bbad9a5da3d08
Samuel Cuenca <ramirezcuenca@outlook.es>
2024-03-25 18:28:43 -0600

    Philosopher class

---
+++ Philosophers/Philosopher.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Philosophers
+{
+    class Philosopher
+    {
+        int id = 0;
+        int meals = 0;
+        char state = 'W'; // W = Waiting, E = Eating
+
+        int forkOne, forkTwo;
+        Semaphore[] forks;
+
+        //Form attributes
+        Label box = null;
+        ListBox listBox = null;
+        Image forkLeftImg = null, forkRightImg = null;
+        PictureBox forkLeft = null, forkRight = null;
+
+        Color waitingColor = Color.FromName("SkyBlue");
+        Color eatingColor = Color.FromName("LimeGreen");
+
+        public Philosopher(int id, int forkOne, int forkTwo, PictureBox forkLeft, PictureBox forkRight, Label box, Semaphore[] forks, ListBox listBox)
+        {
+            this.id = id;
+            this.forkOne = forkOne;
+            this.forkTwo = forkTwo;
+            this.forkLeft = forkLeft;
+            this.forkRight = forkRight;
+            this.box = box;
+            this.forks = forks;
+
+            this.listBox = listBox;
+
+            changeForksStateUI(false);
+            changePhilosopherBoxStateUI(waitingColor);
+        }
+
+        public int getID()
+        {
+           return this.id;
+        } 
+        public int getMeals()
+        {
+            return this.meals;
+        }
+        public void addMeal()
+        {
+            meals = meals + 1;
+        }
+        public char getState()
+        {
+            return this.state;
+        }
+        public void setState(char state)
+        {
+            this.state = state;
+        }
+
+        // Form //
+        public void changeState(char state)
+        {
+            // W = Waiting, E = Eating
+
+            switch (state)
+            {
+                case 'W':
+                    changeForksStateUI(false);
+                    changePhilosopherBoxStateUI(waitingColor);
+                    break;
+                case 'E':
+                    changeForksStateUI(true);
+                    changePhilosopherBoxStateUI(eatingColor);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void changeForksStateUI(bool active)
+        {
+
+            switch (id)
+            {
+                case 0:
+                    forkLeftImg = active ? Filosofos.Properties.Resources.Fork4A : Filosofos.Properties.Resources.Fork4;
+                    forkRightImg = active ? Filosofos.Properties.Resources.Fork0A : Filosofos.Properties.Resources.Fork0;
+                    break;
+                case 1:
+                    forkLeftImg = active ? Filosofos.Properties.Resources.Fork0A : Filosofos.Properties.Resources.Fork0;
+                    forkRightImg = active ? Filosofos.Properties.Resources.Fork1A : Filosofos.Properties.Resources.Fork1;
+                    break;
+                case 2:
+                    forkLeftImg = active ? Filosofos.Properties.Resources.Fork1A : Filosofos.Properties.Resources.Fork1;
+                    forkRightImg = active ? Filosofos.Properties.Resources.Fork2A : Filosofos.Properties.Resources.Fork2;
+                    break;
+                case 3:
+                    forkLeftImg = active ? Filosofos.Properties.Resources.Fork2A : Filosofos.Properties.Resources.Fork2;
+                    forkRightImg = active ? Filosofos.Properties.Resources.Fork3A : Filosofos.Properties.Resources.Fork3;
+                    break;
+                case 4:
+                    forkLeftImg = active ? Filosofos.Properties.Resources.Fork3A : Filosofos.Properties.Resources.Fork3;
+                    forkRightImg = active ? Filosofos.Properties.Resources.Fork4A : Filosofos.Properties.Resources.Fork4;
+                    break;
+            }
+
+            this.forkLeft.Image = forkLeftImg;
+            this.forkRight.Image = forkRightImg;
+        }
+        private void changePhilosopherBoxStateUI(Color color)
+        {
+            box.ForeColor = color;
+        }
+        public void updateMeals()
+        {
+            listBox.Items[id] = ($"Filósofo {id + 1} Comidas: {meals}");
+        }
+    }
+}
